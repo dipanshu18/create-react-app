d3a89287ce557f43414d3cd03668cad2ba80f798
dipanshu18 <torawanedipanshu@gmail.com>
2023-02-14 11:12:30 +0530

    removed repeated Employee and replaced with .map()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./index.css";
 import Employee from "./components/Employee.js";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
 	const showEmployees = true;
 	const [role, setRole] = useState("dev");
+	const [employees, setEmployees] = useState([
+		{
+			name: "Dipanshu",
+			role: "Web Dev",
+			img: "https://images.pexels.com/photos/1666779/pexels-photo-1666779.jpeg",
+		},
+		{
+			name: "Vrukshali",
+			role: "The devops girl",
+			img: "https://images.pexels.com/photos/4355346/pexels-photo-4355346.jpeg",
+		},
+		{
+			name: "Tony",
+			role: "PHP guy",
+			img: "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg",
+		},
+		{
+			name: "Moin",
+			role: "Intern",
+			img: "https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg",
+		},
+		{
+			name: "Kunal",
+			role: "DevRel",
+			img: "https://images.pexels.com/photos/4640886/pexels-photo-4640886.jpeg",
+		},
+		{
+			name: "Vijay",
+			role: "HR",
+			img: "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg",
+		},
+	]);
 	return (
 		<div>
 			<header className="bg-red-400 text-center py-8">
@@ -12,21 +45,30 @@ function App() {
 			</header>
 			<section className="text-center py-8 bg-red-300">
 				{showEmployees ? (
-					<div>
-						Enter role: <input
+					<div className="flex flex-wrap justify-center">
+						{/* Enter role: <input
 							className="bg-slate-400 rounded-md"
 							type="text"
 							onChange={(e) => {
 								console.log(e.target.value);
 								setRole(e.target.value);
 							}}
-						/>
-						<Employee name="Dipanshu" role="newbie" />
-						<Employee name="Vrukshali" role={role} />
-						<Employee name="Tony" />
+						/> */}
+						{employees.map((employee) => {
+							return (
+								<Employee
+									key={uuidv4()}
+									name={employee.name}
+									role={employee.role}
+									img={employee.img}
+								/>
+							);
+						})}
 					</div>
 				) : (
-					<h3 className="bg-red-800 text-white py-10 text-center">You don't have access to the Employees.</h3>
+					<h3 className="bg-red-800 text-white py-10 text-center">
+						You don't have access to the Employees.
+					</h3>
 				)}
 			</section>
 		</div>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,8 +1,20 @@
 function Employee(props) {
 	return (
-		<div>
-			<h3>Employee {props.name}</h3>
-			{props.role ? <p>{props.role}</p> : <p>No role assigned</p>}
+		<div className="py-8 px-8 max-w-sm my-4 mx-4 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+			<img
+				className="block h-[100px] w-[100px] mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+				src={props.img}
+				alt="Woman's Face"
+			/>
+			<div className="text-center space-y-2 sm:text-left">
+				<div className="space-y-0.5">
+					<p className="text-lg text-black font-semibold">{props.name}</p>
+					<p className="text-slate-500 font-medium">{props.role}</p>
+				</div>
+				<button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+					Edit
+				</button>
+			</div>
 		</div>
 	);
 }
