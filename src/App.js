a2fe938633da9cee4b96a9747c95f3fb66e825c2
dipanshu18 <torawanedipanshu@gmail.com>
2023-02-14 10:20:35 +0530

    completed useState section & started tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import "./App.css";
+import "./index.css";
 import Employee from "./components/Employee.js";
+import { useState } from "react";
 
 function App() {
 	const showEmployees = true;
+	const [role, setRole] = useState("dev");
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div>
+			<header className="bg-red-400 text-center py-8">
 				<h1>Hello User!</h1>
 			</header>
-			<section>
-				{console.log("Inside the return")}
+			<section className="text-center py-8 bg-red-300">
 				{showEmployees ? (
 					<div>
+						Enter role: <input
+							className="bg-slate-400 rounded-md"
+							type="text"
+							onChange={(e) => {
+								console.log(e.target.value);
+								setRole(e.target.value);
+							}}
+						/>
 						<Employee name="Dipanshu" role="newbie" />
-						<Employee name="Vrukshali" />
+						<Employee name="Vrukshali" role={role} />
 						<Employee name="Tony" />
 					</div>
 				) : (
-					<h3>You don't have access to the Employees.</h3>
+					<h3 className="bg-red-800 text-white py-10 text-center">You don't have access to the Employees.</h3>
 				)}
 			</section>
 		</div>
