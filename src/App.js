fff4d40a14621f9f5053555fcee97294d958ba3c
dipanshu18 <torawanedipanshu@gmail.com>
2023-02-14 09:25:10 +0530

    about to start props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
+import "./App.css";
+import Employee from "./components/Employee.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
-  );
+	const showEmployees = true;
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Hello User!</h1>
+			</header>
+			<section>
+				{console.log("Inside the return")}
+				{showEmployees ? (
+					<div>
+						<Employee />
+						<Employee />
+						<Employee />
+						<Employee />
+						<Employee />
+						<Employee />
+					</div>
+				) : (
+					<h3>You don't have access to the Employees.</h3>
+				)}
+			</section>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Employee.js
@@ -0,0 +1,5 @@
+function Employee() {
+	return <h3>Here are the Employees!</h3>;
+}
+
+export default Employee;
